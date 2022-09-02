45d2f72d7629ed78471df990ac955e8a27346a55
Carlos David Cruz Zuluaga <70548247+klich404@users.noreply.github.com>
2022-09-01 21:13:36 -0500

    Implementando metodo create

---
+++ Controllers/InventarioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductionSystem.Models;
+
+namespace ProductionSystem.Controllers
+{
+    public class InventarioController : Controller
+    {
+        private readonly ProductionSystemContext _db;
+
+        public InventarioController(ProductionSystemContext db) //carga la base de datos
+        {
+            _db = db;
+        }
+
+        public IActionResult Inventario() //carga la informacion de las tablas hacia la vista Index
+        {
+            ViewBag.Colores = _db.Color.ToList();
+            ViewBag.Telas = _db.TipoTelas.ToList();
+            ViewBag.Inventario = _db.Inventario.ToList();
+            ViewBag.MateriaPrima = _db.MateriaPrima.ToList();
+            return View();
+        }
+    }
+}
